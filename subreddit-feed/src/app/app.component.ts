4841386484c4cbfc2e5e3b0b21638754ad3bf6fd
Gustaf Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-10-02 14:37:25 +0200

    Removed previous code for http request, opting to follow an initial guide found online going forward to set in place majority of core functionality. Guide has been cross-checked with Angular course developer is currently enrolled in to verify it's logic

--- subreddit-feed/src/app/app.component.ts
+++ subreddit-feed/src/app/app.component.ts
@@ -8,23 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  baseURL = 'https://www.reddit.com/r/';
-  subreddit = 'sweden.json';
-  query = '10'
-
-  constructor(private http: HttpClient){
-    this.http.get(this.baseURL).toPromise().then(data => {
-      console.log(data);
-    }
-    )
-  }
-//   getPosts(subreddit, postCount): Observable <any> {
-//     return this.http.get(`${this.baseURL}${subreddit}?limit=${postCount}`)
-// }
-
-// loadPosts() {
-//   this.apiService.getPosts('funny', '6').subscribe((resp) => {
-//       console.log(resp)
-//   }
-// ,}
+  title = 'My Subreddit feed'
 }
